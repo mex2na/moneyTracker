4b0eb4418ef0bea4d3e64b2795c11ceb3aa6e217
noelardau <noelardau@gmail.com>
2024-11-21 10:47:55 +0300

    FIX - route /user

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller({
-  path: "user"
-  //même chose que path tsotra ihany 
-})
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
